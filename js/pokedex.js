ce8899b926c0cbd396626298a8ce7a106f249e0c
Liann Sun <lianndabomb@yahoo.com>
2017-01-04 19:39:35 +0000

    some updates

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,27 +1,27 @@
 $(function() {
   // Step 0: Set your assigned pokemon to the "pokemon" variable! I have put in Bulbasaur for an example
-  var pokemon = bulbasaur;
+  var pokemon = charmander;
 
   // STEP 1: Uncomment the next line and make sure that the pokemon object prints to the console!
   //   e.g. console.log(pokemon)
 
   // STEP 2: Use jQuery to add information to the empty fields. The first few have been done for you.
-
+  // Note that some of these fields may not exist for your pokemon. 
+  
   $(".pokemon-name").append(pokemon.name);
-
-
-  $(".pokemon-species").append("something");
-  $(".pokemon-evolves").append("something else");
-
+  $(".pokemon-evolves").append(pokemon.evolutions[0].to);
+  $(".pokemon-ev-level").append("?") // Replace the ?
   // TODO: Add evolution level here
   // TODO: Add speed here
   // TODO: Add attack here
   // TODO: Add defense here
-  // TODO: Add ability names here
-  // TODO: Add egg group names here
+  
+  // TODO: Add ability names here to the <ul> with class .ability-names
+  
+  // TODO: Add egg group names here to a <ul> that you created
 
-  // TODO: Add move names in alphabetical order here
-  // TODO: Add ONLY level up move names here
+  // TODO: Add move names in alphabetical order here to the existing <ul>
+  // TODO: Add ONLY level up move names to the <ul> you created
 
   // Now... take a deep breath and go back to your move lists.
   // TODO: Find a way to use variables to make your code more readable!
